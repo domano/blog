521d780238e389df1a892b156de1f0aef344d71c
Dino Omanovic <angor.mail@googlemail.com>
2017-12-04 22:31:24 +0100

    Clean up boilerplate stuff

--- blog-fe/src/index.js
+++ blog-fe/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Blog from './components/blog';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Blog />, document.getElementById('root'));
 registerServiceWorker();
