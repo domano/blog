fca7fa42b610b76beaa6890578e311cc5413dcaa
Dino Omanovic <angor.mail@googlemail.com>
2017-12-06 21:29:08 +0100

    split up components and started work on the landing page

--- blog-fe/src/components/blogLanding.js
+++ blog-fe/src/components/blogLanding.js
@@ -9,26 +9,32 @@ import Subscribe from './subscribe'
 import BlogPost from './blogPost'
 import { Parallax } from 'react-parallax'
 import background from '../assets/brandi-redd-122054.jpg'
-import { Grid, Row, Col, Navbar, Well, Nav, NavItem, Button, NavDropdown, FormGroup, InputGroup, FormControl, MenuItem, PanelGroup, Panel, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Grid, Row, Jumbotron, Col, Navbar, Well, Nav, NavItem, Button, NavDropdown, FormGroup, InputGroup, FormControl, MenuItem, PanelGroup, Panel, ListGroup, ListGroupItem } from 'react-bootstrap'
 import ReactMarkdown from 'react-markdown'
 
 let bgStyle = {
   backgroundColor: 'rgba(245, 245, 245, 0.4)'
 }
 
-export default ({links, blogPost}) => <Parallax bgImage={background} strength={400}>
+export default ({teasers, links}) => <Parallax bgImage={background} strength={400}>
   <Grid style={bgStyle}>
     <Row>
       <Header />
     </Row>
     <Row>
       <Col md={8}>
-        <BlogPost input={blogPost} />
+        {teasers.map((teaser) => 
+        <Jumbotron ><Well>
+          <h2>{teaser.title}</h2>
+        <p>{teaser.description}</p>
+        <p><Button block bsStyle="primary" href={teaser.href}>Read</Button></p>
+        </Well></Jumbotron>
+        )}
       </Col>
       <Col md={4} >
-        <PanelGroup>
-        <RecentPosts links={links}/>
-        <Subscribe />
+        <PanelGroup defaultActiveKey="2">
+          <RecentPosts links={links}/>
+          <Subscribe />
         </PanelGroup>
       </Col>
     </Row>
@@ -37,3 +43,4 @@ export default ({links, blogPost}) => <Parallax bgImage={background} strength={4
     </Row>
   </Grid>
 </Parallax>
+
