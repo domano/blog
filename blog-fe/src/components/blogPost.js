4cf3e98924d23de07723f9f1581368689f4941d5
Dino Omanovic <angor.mail@googlemail.com>
2017-12-14 19:40:22 +0100

    Remove frontend from this repo

--- blog-fe/src/components/blogPost.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Grid, Row, Col, Navbar, Well, Nav, NavItem, Button, NavDropdown, FormGroup, InputGroup, FormControl, MenuItem, PanelGroup, Panel, ListGroup, ListGroupItem } from 'react-bootstrap'
-import ReactMarkdown from 'react-markdown'
-
-export default ({input}) =>
-  <Panel>
-    <ReactMarkdown source={input} />
-  </Panel>
--- blog-fe/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Blog from './components/blog';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Blog />, document.getElementById('root'));
-registerServiceWorker();
